5196e1562ae196f241d5149d9d22d6e1c081754d
Mat-vieira12 <mateus.vieirasenai2024@gmail.com>
2024-04-23 16:38:11 -0300

    criacao variaveis.py

---
+++ Variáveis.py
@@ -0,0 +1,13 @@
+print ("Olá Mundo!")
+Nome="Mateus Vieira"
+idade=16
+email='mateus.vieira@gmail.com'
+telefone='21 99874-5129'
+nota=9
+aprovado=True
+print(Nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
