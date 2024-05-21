3f860ae3056053ec21afc8f361ed0e28394e2a29
Mat-vieira12 <mateus.vieirasenai2024@gmail.com>
2024-05-21 14:54:56 -0300

    criacao.py

---
+++ Listas.py
@@ -0,0 +1,8 @@
+aluno = ["Mateus Vieira", 4, 9.5, True]
+print(f'Nome:{aluno[0]}')
+print(f'Falta:{aluno[1]}')
+print(f'Média:{aluno[2]}')
+if aluno[3] == True:
+    print(f'Aprovado: Sim')
+else:
+    print(f'Aprovado: Não')
